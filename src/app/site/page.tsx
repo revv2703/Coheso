ce6555a700c941854f2d1054ab4e1da878af158e
revv2703 <asijaprakash27@gmail.com>
2024-03-13 18:27:32 +0530

    Added pricing section

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -1,11 +1,22 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { pricingCards } from "@/lib/constants";
+import clsx from "clsx";
 import Image from "next/image";
+import { Check } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <section className="h-full w-full pt-36 relative flex items-center justify-center flex-col">
-        <div className="absolute bottom-0 left-0 right-0 top-0 dark:bg[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:4rem_4rem]" />
-        <div className="absolute left-0 right-0 top-[-10%] h-[900px] w-[900px] rounded-full dark:bg[radial-gradient(circle_400px_at_50%_300px,#fbfbfb36,#000)]" />
+        <div className="absolute inset-0 -z-10 h-full w-full items-center px-50 py-24 dark:[background:radial-gradient(125%_125%_at_50%_10%,#000_30%,#905_200%)] [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#64e_100%)]" />
         <p className="text-center">Run your agency, in one place</p>
         <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
           <h1 className="text-9xl font-bold text-center md:text-[300px]">
@@ -16,11 +27,63 @@ export default function Home() {
           <Image
             src="/assets/preview.png"
             alt="Coheso"
-            width={120}
-            height={120}
+            width={1200}
+            height={1200}
             className="rounded-tl-2xl rounded-tr-2xl border-2 border-muted"
           />
           <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div>
+        </div>
+      </section>
+      <section className="flex justify-center items-center flex-col gap-4 md:!mt-20 mt-[-60px]">
+        <h2 className="text-4xl text-center">Choose what fits you right.</h2>
+        <p className="text-muted-foreground text-center">
+          Our straightforward pricing plans are tailored to meet your needs. If
+          {" you're"} not <br />
+          ready to commit you can get started for free.
+        </p>
+        <div className="flex justify-center gap-4 flex-wrap mt-6">
+          {pricingCards.map((card) => (
+            <Card
+              key={card.title}
+              className={clsx("w-[300px] flex flex-col justify-between", {
+                "border-2 border-primary": card.title === "Unlimited Saas",
+              })}
+            >
+              <CardHeader>
+                <CardTitle
+                  className={clsx("", {
+                    "text-muted-foreground": card.title !== "Unlimited Saas",
+                  })}
+                >
+                  {card.title}
+                </CardTitle>
+                <CardDescription>{card.description}</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <span className="text-4xl font-bold">{card.price}</span>
+                <span className="text-muted-foreground"> every month</span>
+              </CardContent>
+              <CardFooter className="flex flex-col items-start gap-4">
+                <div>
+                  {card.features.map((feature) => (
+                    <div key={feature} className="flex gap-2 items-center">
+                      <Check className="text-muted-foreground" />
+                      <p>{feature}</p>
+                    </div>
+                  ))}
+                </div>
+                <Link
+                  href={`/agency?plan=${card.priceId}`}
+                  className={clsx(
+                    "w-full text-center bg-primary p-2 rounded-lg",
+                    { "!bg-muted-foreground": card.title !== "Unlimited Saas" }
+                  )}
+                >
+                  Get started!
+                </Link>
+              </CardFooter>
+            </Card>
+          ))}
         </div>
       </section>
     </>
---
+++ src/lib/constants.ts
@@ -0,0 +1,29 @@
+export const pricingCards = [
+    {
+      title: 'Starter',
+      description: 'Perfect for trying out Coheso',
+      price: 'Free',
+      duration: '',
+      highlight: 'Key features',
+      features: ['3 Sub accounts', '2 Team members', 'Unlimited pipelines'],
+      priceId: '',
+    },
+    {
+      title: 'Unlimited Saas',
+      description: 'The ultimate agency kit',
+      price: '₹10000',
+      duration: 'month',
+      highlight: 'Key features',
+      features: ['Everything from Basic Plan', 'Rebilling', '24/7 Support team'],
+      priceId: 'price_1OYxkqFj9oKEERu1KfJGWxgN',
+    },
+    {
+      title: 'Basic',
+      description: 'For serious agency owners',
+      price: '₹5000',
+      duration: 'month',
+      highlight: 'Everything in Starter, plus',
+      features: ['Everything from Starter Plan','Unlimited Sub accounts', 'Unlimited Team members'],
+      priceId: 'price_1OYxkqFj9oKEERu1NbKUxXxN',
+    },
+  ]
